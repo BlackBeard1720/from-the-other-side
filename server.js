44c8292d02941d88be1e13507b1a358f512f7208
Minh <223230201+BlackBeard1720@users.noreply.github.com>
2026-06-13 14:03:50 +0700

    add static file server with error handling

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import http from 'node:http';
+import path from 'node:path';
+import { serveStatic } from './utils/serveStatic.js';
 
 const __dirname = import.meta.dirname;
-const server = http.createServer((req, res) => {
-   res.statusCode = 200;
-   res.setHeader('Content-Type', 'text/html');
-   res.end();
+const server = http.createServer(async (req, res) => {
+    await serveStatic(req, res, __dirname);
 });
 
 server.listen(3000, () => {
